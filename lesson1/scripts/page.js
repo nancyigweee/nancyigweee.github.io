53c242aff686329338e2de78b44a001d39ce7cb0
nancyigweee <nancyigweee@gmail.com>
2021-06-17 02:03:48 -0600

    designprinciples

--- lesson1/scripts/page.js
+++ lesson1/scripts/page.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', (event)=> {
-    const lu = document.querySelector('#lastupdated');
+    const lu = document.querySelector("#lastupdated");
     lu.textContent = document.lastModified;
 
-    const cry = document.querySelector("copyrightyear");
+    const cry = document.querySelector("#copyrightyear");
     cry.textContent = new Date().getFullYear();
 })
